b2645d28e80ff5b621773fed653566bc04e381ec
TrnCngH <hautrancong@gmail.com>
2022-08-16 22:34:26 +0700

    adding admin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import ROUTES from './app/ROUTES';
 import './App.css';
+import Administator from './app/Administrator'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+      <header>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={ROUTES.adminRoute()} element={<Administator />}
+            ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/app/Administrator.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Administator = () => {
+  return (
+    <div>this is admin page</div>
+  )
+}
+Administator.propTypes = {
+
+}
+
+export default Administator

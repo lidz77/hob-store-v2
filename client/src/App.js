bfba293b6331e124b46747fdfdf9bbd3a1766df5
TrnCngH <hautrancong@gmail.com>
2022-08-17 12:54:44 +0700

    finished admin layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,15 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import ROUTES from './app/ROUTES';
 import './App.css';
-import Administator from './app/Administrator'
+import Administration from './app/Administration'
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
       <BrowserRouter>
         <Routes>
           <Route
-            path={ROUTES.adminRoute()} element={<Administator />}
+            path={ROUTES.adminRoute()} element={<Administration />}
             ></Route>
         </Routes>
       </BrowserRouter>
